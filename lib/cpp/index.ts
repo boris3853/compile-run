e2cb55f1fa3ca34f1020e7d9c1a11e3e79c85f4d
Andrew Kyung <60434800+boris3853@users.noreply.github.com>
2022-10-07 15:24:38 +0900

    Update index.ts

--- lib/cpp/index.ts
+++ lib/cpp/index.ts
@@ -1,7 +1,12 @@
+import { compileCpp } from './compile-file';
+import { compileCppSource } from './compile-source';
 import { runCppFile } from './run-file';
 import { runCppSource } from './run-source';
 
+
 const cpp = {
+    compileFile: compileCpp,
+    compileSource: compileCppSource,
     runFile: runCppFile,
     runSource: runCppSource
 };
